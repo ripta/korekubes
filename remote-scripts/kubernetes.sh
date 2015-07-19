744d43a44617ba001978d27f9e922546a0920b0a
Ripta Pasay <github@r8y.org>
2015-07-19 15:34:11 -0700

    kubernetes: remove old tar.gz method of distribution

--- remote-scripts/kubernetes.sh
+++ remote-scripts/kubernetes.sh
@@ -26,9 +26,3 @@ download_url $TMPDIR setup-network-environment "https://github.com/kelseyhightow
 sudo mkdir -p /opt/bin
 sudo cp -p $TMPDIR/* /opt/bin/
 sudo chown root:root -R /opt/bin
-
-sudo tar zxf /tmp/kubernetes.tar.gz -C /opt/bin
-sudo chmod +x /opt/bin/*
-sudo chown root:root -R /opt/bin
-sudo tar zxf /tmp/services.tar.gz -C /etc/systemd/system
-rm -f /tmp/*.tar.gz
