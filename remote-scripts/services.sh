90a9d1f0b308fad223d4cab9086962e808582c21
Ripta Pasay <github@r8y.org>
2015-07-19 18:54:23 -0700

    generate: fix typo

--- remote-scripts/services.sh
+++ remote-scripts/services.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo mv /tmp/services/*.services /etc/systemd/system
+sudo mv /tmp/services/*.service /etc/systemd/system
 sudo chown -R root:root /etc/systemd/system
 rm -r /tmp/services
